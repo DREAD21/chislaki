a1bea5618737253b25379e4b5d7055cb0823fa1b
DREAD21 <87900013+DREAD21@users.noreply.github.com>
2022-03-27 14:57:57 +0200

    Add files via upload

---
+++ labc2.1/labc2.1/Exp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace labc2._1
+{
+    class Exp
+    {
+        public static double e;
+        public static async void readFile()
+        {
+            string path = @"C:\Users\Никита\Desktop\числаки\labc2.1\2.1.txt";
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                await fstream.ReadAsync(buffer, 0, buffer.Length);
+                string textFromFile = Encoding.Default.GetString(buffer);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+                e = Convert.ToDouble(textFromFile, System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ labc2.1/labc2.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace labc2._1
+{
+    class Decision : Exp
+    {
+        public static double x0 ;
+        public static double x1 ;
+        public static double q;
+        public static void Iterations(out double answer)
+        {
+            q = get_q();
+            x0 = 0.35;
+            x1 = 0.35;
+            do
+            {
+                x0 = x1;
+                x1 = function_iter(x0);
+            }
+            while (q/(1-q)*Math.Abs(x1 - x0) > e);
+            answer = x1; 
+            
+        }
+        public static void Newton(out double answer)
+        {
+            x0 = 0.35;
+            x1 = 0.35;
+            do
+            {
+                x0 = x1;
+                x1 = x0 - function(x0) / derivative(x1);
+            } while (Math.Abs(x1 - x0) > e || x1 < 0);
+            answer = x1;
+        }
+        public static double function(double arg)
+        {
+            return Math.Pow(2, arg) + Math.Pow(arg, 2) - 2;
+        }
+        public static double function_iter(double arg)
+        {
+            return Math.Pow((2-Math.Pow(2,arg)),0.5);
+        }
+        public static double derivative(double arg)
+        {
+            return Math.Pow(2, arg) * Math.Log(2) + 2 * arg;
+        }
+        public static double get_q()
+        {
+            return Math.Max(Math.Abs(function_iter(0)), Math.Abs(function_iter(0.7)));
+        }
+    }
+    class Program
+    {
+        public static double answer;
+        public static double answer2;
+        static void Main(string[] args)
+        {
+            Exp.readFile();
+            Decision.Newton(out answer);
+            Decision.Iterations(out answer2);
+            Console.WriteLine("Метод Ньютона:  " + answer);
+            Console.WriteLine("Метод простых итераций:  " + answer2);
+            Console.ReadKey();
+        }
+    }
+}
