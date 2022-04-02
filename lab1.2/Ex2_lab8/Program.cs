6289bec9795b16e83fdeb23013a19d9569111974
DREAD21 <87900013+DREAD21@users.noreply.github.com>
2022-04-02 20:18:53 +0300

    Update Program.cs

--- lab1.2/Ex2_lab8/Program.cs
+++ lab1.2/Ex2_lab8/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,12 +7,11 @@ namespace Ex2_lab8
 {
     class Task
     {
-        //Получить матрицу из консоли
         public double[,] GetMat()
         {
-            string[] lines = File.ReadAllLines(@"C:\Users\Никита\Desktop\числаки\1.2.txt").Take(5).ToArray();
+            string[] lines = File.ReadAllLines(@"C:\Users\Никита\Desktop\числаки\lab1.2\1.2.txt").Take(5).ToArray();
             double[,] matrix = new double[5, 6];
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
                 int[] row = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
                 for (int j = 0; j < 6; j++)
@@ -32,20 +31,46 @@ namespace Ex2_lab8
                 for (int j = 0; j < mat.GetLength(1); j++)
                     Console.Write("{0,8}", mat[i, j]);
         }
-
-
-        public string PolPartToString(double coeff, int pow)
+        public double[] CoefCounter(double[,] mat)
         {
-            if (coeff == 0) return null;
-            if (pow == 0) return coeff.ToString();
-            var cf = coeff.ToString();
-            if (coeff == 1) cf = "";
-            if (coeff == -1) cf = "-";
-            if (pow == 1) return cf + "x";
-            return cf + "x^" + pow.ToString();
-        }
+            double[] Ps = new double[mat.GetLength(0)];
+            double[] Qs = new double[mat.GetLength(0)];
+            double[] x = new double[mat.GetLength(0)];
+            Ps[0] = -mat[0, 1] / mat[0, 0];
+            Qs[0] = mat[0, mat.GetLength(1) - 1] / mat[0, 0];
+            for (int i = 1; i < mat.GetLength(0); i++)
+            {
+                Ps[i] = -mat[i, 1 + i] / (mat[i, i] + mat[i, i - 1] * Ps[i - 1]);
+                Qs[i] = (mat[i, mat.GetLength(1) - 1] - mat[i, i - 1] * Qs[i - 1]) / (mat[i, i] + mat[i, i - 1] * Ps[i - 1]);
+            }
 
-        //Умножение матриц
+            // обратный ход
+            x[mat.GetLength(0) - 1] = Qs[mat.GetLength(0) - 1];
+            for (int j = mat.GetLength(0) - 2; j >= 0; j--)
+            {
+                x[j] = Ps[j] * x[j + 1] + Qs[j];
+            }
+            for (int m = 0; m < mat.GetLength(0); m++)
+            {
+                Console.WriteLine("x{0}={1}", m + 1, Math.Round(x[m], 4));
+            }
+
+            return x;
+
+        }
+        public double[,] Geta(double[,] mat)
+        {
+            double[,] mat1 = new double[mat.GetLength(0), mat.GetLength(0)];
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1) - 1; j++)
+                {
+                    mat1[i, j] = mat[i, j];
+                }
+            }
+            PrintMat(mat1);
+            return mat1;
+        }
         public double[,] MulMat(double[,] mat1, double[,] mat2)
         {
             double[,] res = new double[mat1.GetLength(0), mat2.GetLength(1)];
@@ -59,32 +84,25 @@ namespace Ex2_lab8
                     }
                 }
             }
+            PrintMat(res);
 
             return res;
         }
-        public void CoefCounter(double[,] mat)
+        public double[] Stolb(double[,] A, double[] B)
         {
-            double[] Ps = new double[mat.GetLength(0)];
-            double[] Qs = new double[mat.GetLength(0)];
-            double[] x = new double[mat.GetLength(0)];
-            Ps[0] = -mat[0, 1] / mat[0, 0];
-            Qs[0] = mat[0, mat.GetLength(1) - 1] / mat[0, 0];
-            for (int i=1; i < mat.GetLength(0); i++)
+            double[] res = new double[B.GetLength(0)];
+            for (int row = 0; row < A.GetLength(0); row++)
             {
-                Ps[i] = -mat[i,1+i]/(mat[i,i]+mat[i,i-1]*Ps[i-1]);
-                Qs[i] = (mat[i, mat.GetLength(1) - 1] - mat[i, i - 1] * Qs[i - 1]) / (mat[i,i]+ mat[i, i - 1]*Ps[i-1]);
+                for (int col = 0; col < A.GetLength(1); col++)
+                {
+                    res[col] += A[col,row] * B[row];
+                }
             }
-
-            // обратный ход
-            x[mat.GetLength(0) - 1] = Qs[mat.GetLength(0)-1];
-            for (int j = mat.GetLength(0)-2; j >= 0; j--)
-            {
-                x[j] = Ps[j] * x[j + 1] + Qs[j];
-            }
-            for (int m = 0; m < mat.GetLength(0); m++)
-            {
-                Console.WriteLine("x{0}={1}", m+1, Math.Round(x[m],4));
-            }
+           for(int i = 0; i < res.Length; i++)
+           {
+                Console.WriteLine(res[i]);
+           }
+            return res;
         }
     }
     class Program
@@ -96,7 +114,11 @@ namespace Ex2_lab8
             //string[] input = Console.ReadLine().Split('|');
             var matrix = task.GetMat();
             task.PrintMat(matrix);
-            task.CoefCounter(matrix);
+            double[] c = task.CoefCounter(matrix);
+            double[,] watch = task.Geta(matrix);
+            Console.WriteLine("Проверка");
+            task.Stolb(watch, c);
+            Console.ReadLine();
         }
     }
 }
